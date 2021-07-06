c3526ab891400e8804bc4c426cae787be5ee36a7
BenigneDemetz <72600691+BenigneDemetz@users.noreply.github.com>
2021-07-06 20:11:33 +0200

    gui et speed

--- src/net/deadpvp/Main.java
+++ src/net/deadpvp/Main.java
@@ -1,12 +1,16 @@
 package net.deadpvp;
 
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteStreams;
 import net.deadpvp.commands.DpAccept;
+import net.deadpvp.commands.Speed;
 import net.deadpvp.commands.TestCommand;
 import net.deadpvp.commands.hub;
 import net.deadpvp.events.EventListener;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
@@ -21,26 +25,35 @@ public class Main extends JavaPlugin implements PluginMessageListener {
         instance = this;
         this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         this.getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", this);
+        this.getServer().getMessenger().registerIncomingPluginChannel(this, "deadpvp:return", this);
         PluginManager pm = Bukkit.getServer ().getPluginManager ();
         pm.registerEvents (new EventListener (), this);
         getCommand ("hub").setExecutor (new hub (this));
         getCommand ("dpaccept").setExecutor (new DpAccept ());
-        getCommand ("test").setExecutor (new TestCommand ());
-    
+//        getCommand ("test").setExecutor (new TestCommand ());
+        getCommand ("speed").setExecutor (new Speed());
         new BukkitRunnable () {
             @Override
             public void run() {
                 for(Player pl : Bukkit.getOnlinePlayers ()){
                     pl.setGameMode (GameMode.CREATIVE);
+                    EventListener.hasAccepted.add(pl);
                 }
             }
         }.runTaskLater (Main.getInstance (), 20L);
-        
+
+        for(Player pl : Bukkit.getOnlinePlayers ()){
+            pl.setGameMode (GameMode.CREATIVE);
+            EventListener.hasAccepted.add(pl);
+        }
         super.onEnable ();
     }
     
     @Override
     public void onDisable() {
+        this.getServer().getMessenger().unregisterOutgoingPluginChannel(this, "BungeeCord");
+        this.getServer().getMessenger().unregisterIncomingPluginChannel(this, "BungeeCord", this);
+        this.getServer().getMessenger().unregisterIncomingPluginChannel(this, "deadpvp:return", this);
         
         
         super.onDisable ();
@@ -52,6 +65,13 @@ public class Main extends JavaPlugin implements PluginMessageListener {
     
     @Override
     public void onPluginMessageReceived (String s, Player player, byte[] bytes) {
-    
+        ByteArrayDataInput in = ByteStreams.newDataInput(bytes);
+        String subchannel = in.readUTF();
+        if (subchannel.equals("command")) {
+            String cmd = in.readUTF();
+            System.out.println("[Boutiuqe]");
+            System.out.println(cmd);
+            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), cmd);
+        }
     }
 }
---
+++ src/net/deadpvp/commands/Speed.java
@@ -0,0 +1,108 @@
+package net.deadpvp.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+public class Speed implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (p.hasPermission("deadpvp.creative.speed")) {
+                int walkSpeed = (int) (p.getWalkSpeed()*10);
+                int flySpeed = (int) (p.getFlySpeed()*10);
+                if (args.length >= 1) {
+                    try {
+                        if (args[0].contains("reset")) {
+                            p.setFlySpeed((float) 0.1);
+                            p.setWalkSpeed((float) 0.2);
+                            p.sendMessage("§eVitesse reset");
+                            return true;
+                        }
+                        if (Float.parseFloat(args[0]) > 10) return false;
+
+                        else if (p.hasPermission("deadpvp.architecte"));
+
+                        else if (p.hasPermission("deadpvp.constructeur")) {
+                            if (Float.parseFloat(args[0]) > 6) {
+                                p.sendMessage("§cTu ne peux pas dépasser la vitesse de 6");
+                                return true;
+                            }
+                        }
+
+                        else if (p.hasPermission("deadpvp.apprenti")) {
+                            if (Float.parseFloat(args[0]) > 4 ) {
+                                p.sendMessage("§cTu ne peux pas dépasser la vitesse de 4");
+                                return true;
+                            }
+                        }
+
+
+
+                        if (p.isFlying()) {
+                            p.setFlySpeed(Float.parseFloat(args[0]) / 10);
+                            p.sendMessage("§bSpeed en vol : §6 " + flySpeed + " -> " + args[0]);
+                        } else if (p.isOnGround()) {
+                            p.setWalkSpeed(Float.parseFloat(args[0]) / 10);
+                            p.sendMessage("§bSpeed au sol : §6" + walkSpeed + " -> " + args[0]);
+                        }
+                    }
+                    catch (NumberFormatException  e)
+                    {
+                        System.out.println(e);
+                        try {
+                            if (Float.parseFloat(args[0]) > 10) return false;
+
+                            else if (p.hasPermission("deadpvp.architecte"));
+
+                            else if (p.hasPermission("deadpvp.constructeur")) {
+                                if (Float.parseFloat(args[0]) > 6) {
+                                    p.sendMessage("§cTu ne peux pas dépasser la vitesse de 6");
+                                    return true;
+                                }
+                            }
+
+                            else if (p.hasPermission("deadpvp.apprenti")) {
+                                if (Float.parseFloat(args[0]) > 4 ) {
+                                    p.sendMessage("§cTu ne peux pas dépasser la vitesse de 4");
+                                    return true;
+                                }
+                            }
+
+
+
+                            if (p.isFlying()) {
+                                p.setFlySpeed(Float.parseFloat(args[0]) / 10);
+                                p.sendMessage("§bSpeed en vol : §6 " + flySpeed + " -> " + args[0]);
+                            } else if (p.isOnGround()) {
+                                p.setWalkSpeed(Float.parseFloat(args[0]) / 10);
+                                p.sendMessage("§bSpeed au sol : §6" + walkSpeed + " -> " + args[0]);
+                            }
+                        }
+                        catch (Exception ee)
+                        {
+                            System.out.println(ee);
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                else {
+                    p.sendMessage("§bTu vol en vitesse : §6" + flySpeed +
+                            " §7(1 par défault)\n§bTu marches en vitesse : §6" + walkSpeed + " §7(2 par défault)");
+                    return true;
+                }
+            }
+            else sender.sendMessage("§cTu n'as pas la permission d'utiliser cette commande !");
+        }
+        else sender.sendMessage("Tu n'es pas un joueur");
+
+
+        return true;
+    }
+}
